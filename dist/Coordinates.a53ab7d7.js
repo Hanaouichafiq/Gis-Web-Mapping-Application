59546331f2d168e05ef64aabad1647c5d3ff4c90
HANAOUI Chafiq <chafiq.512@gmail.com>
2021-09-18 20:28:32 +0100

    first commit

--- dist/Coordinates.a53ab7d7.js
+++ dist/Coordinates.a53ab7d7.js
@@ -6509,7 +6509,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62983" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65112" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
